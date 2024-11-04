928a591c4f48bb86de33c435fba1d4a96cb26efc
Luke Swaby <lukeswabypetts@gmail.com>
2024-11-04 19:35:54 +0000

    torchvision model repo added

--- AML/models/vision/__init__.py
+++ AML/models/vision/__init__.py
@@ -4,7 +4,7 @@ from torchvision.models import *
 
 # Register all torchvision models
 for model_name in list_models():
-    model_class = get_model_builder(model_name)
-    MODEL_REGISTRY.register(model_name)(model_class)
+    model_fn = get_model_builder(model_name)
+    MODEL_REGISTRY.register(model_name)(model_fn)
 
-del model_name, model_class
+del model_name, model_fn
