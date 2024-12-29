9feaccb4273bad9fabe2cc6ec8d656043cf4d196
Luke Swaby <lukeswabypetts@gmail.com>
2024-12-29 17:55:18 +0000

    high-level register added so all available models etc can be accessed easily

---
+++ AML/__init__.py
@@ -0,0 +1,15 @@
+from AML.datasets import DATASET_REGISTRY
+from AML.loss import LOSS_REGISTRY
+from AML.metrics import METRIC_REGISTRY
+from AML.models import MODEL_REGISTRY
+from AML.callbacks import CALLBACK_REGISTRY
+from AML.utils.data.splitters import DATA_SPLITTER_REGISTRY
+
+register = {
+    'datasets': DATASET_REGISTRY.list_keys(),
+    'data_splitters': DATA_SPLITTER_REGISTRY.list_keys(),
+    'losses': LOSS_REGISTRY.list_keys(),
+    'models': MODEL_REGISTRY.list_keys(),
+    'metrics': METRIC_REGISTRY.list_keys(),
+    'callbacks': CALLBACK_REGISTRY.list_keys(),
+}
